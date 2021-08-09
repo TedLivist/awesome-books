66184814ab4ae65ff0bc8c57897e1509fc832075
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-08-09 09:47:21 +0100

    Load books from localStorage

--- main.js
+++ main.js
@@ -42,3 +42,31 @@ function addItem(title, author) {
 buttonClass.addEventListener('click', () => {
   addItem(bookTitle, bookAuthor);
 });
+
+// GETTING FORM DATA FROM LOCAL STORAGE
+window.addEventListener('load', () => {
+  const reloadBooks = JSON.parse(localStorage.getItem('books'));
+  if (reloadBooks !== null) {
+    reloadBooks.forEach((abook) => {
+      const book = document.createElement('li');
+      const deleteBtn = document.createElement('button');
+      deleteBtn.innerText = 'Remove';
+      book.innerHTML = `<p>${abook.title}</p>
+    <p>${abook.author} </p>`;
+      deleteBtn.id = abook.title;
+      deleteBtn.className = 'removeBtn';
+      const br = document.createElement('br');
+      list.appendChild(book);
+      book.appendChild(deleteBtn);
+      list.appendChild(br);
+      deleteBtn.addEventListener('click', () => {
+        if (deleteBtn.id === abook.title) {
+          const index = reloadBooks.findIndex((rBook) => rBook.title === deleteBtn.id);
+          reloadBooks.splice(index, 1);
+          list.removeChild(book);
+          saveBooks(reloadBooks);
+        }
+      });
+    });
+  }
+});
