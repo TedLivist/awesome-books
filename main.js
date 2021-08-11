b3b69958ffe716b520cd6265a37c8d723dbdc838
elmar8287 <L-mar@inbox.ru>
2021-08-11 11:54:03 +0400

    Create menthods for creation, saving and finding books

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ class Book {
     this.author = author;
   }
 }
+
+class UseBook {
+  static createBook() {
+    return new Book(bookTitle.value, bookAuthor.value);
+  }
+
+  static saveBook(newBook) {
+    const books = JSON.parse(localStorage.getItem('books'));
+    if (books === null) {
+      localStorage.setItem('books', JSON.stringify([]));
+    } else {
+      books.push(newBook);
+      localStorage.setItem('books', JSON.stringify(books)); //
+    }
+  }
+
+  static findBooks() {
+    return JSON.parse(localStorage.getItem('books'));
+  }
